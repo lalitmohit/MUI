464063f7dbcac3f9c6ebe3d848f2791ae1428d32
Lalit Gour <lalitg@iitbhilai.ac.in>
2024-07-04 23:52:55 +0530

    Updated

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -36,6 +36,49 @@ const Projects = () => {
 
     const l = [
         {
+            name: "Aoushadhi- An Ecommerce Application",
+            image: Aoushadhi,
+            content: "I led the development of a feature-rich online pharmacy platform similar to Netmeds, creating a user-friendly interface with React.js for easy navigation, collaborating with vendors to upload detailed medicine information, integrating secure payment gateways, and leveraging Node.js and Express.js for a robust backend to manage authentication, authorization, and database operations.",
+            github:"https://github.com/lalitmohit/E-comm",
+            visit:"http://aoushadhi.in/"
+        },
+        {
+            name: "TA Management System",
+            image: TA,
+            content: "I led the development of the TA Management System, integrating frontend, backend, and ML models, designed to assist professors in selecting suitable TAs, establishing user authentication, categorizing user information for efficient storage, implementing a user-friendly interface with React.js, and developing an ML model using Metaflow, Sentence-transformer, and NLP for accurate TA assessment.",
+            github:"https://github.com/kunalthgr8/TA_Managment",
+            visit:"https://ta-managment.vercel.app/"
+        },
+        {
+            name: "Emotion Prediction by Text",
+            image: Emotion,
+            content: "I employed CNN and NLP techniques to develop a project that predicts emotions from textual data, utilizing advanced machine learning algorithms for accurate emotion prediction.",
+            github:"https://github.com/lalitmohit/Emotion-Detection-by-Text",
+            visit:"https://emotion-detection-by-text-2r7exvwxrtjbzdsh9xr8l5.streamlit.app/"
+        },
+        {
+            name: "Web App for OpenLake",
+            image: Openlake,
+            content: "I developed a web application for Openlake, an open-source club at IIT Bhilai, using HTML, CSS, JavaScript, React, and FAST API to enhance user experience and engagement within the Openlake community through intuitive and responsive web functionalities.",
+            github:"https://github.com/lalitmohit/OpeLake-Website-by-DevDynamos",
+            visit:"https://webwave-test.vercel.app/"
+        },
+        {
+            name: "MY Portfolio",
+            image: Portfolio,
+            content: "I developed my portfolio using React and Material-UI, showcasing my projects and skills in a visually appealing, responsive design. This project demonstrates my expertise in front-end development, intuitive UI creation, and proficiency with modern web technologies.",
+            github:"https://github.com/lalitmohit/MUI",
+            visit:"https://mui-seven-chi.vercel.app/"
+        },
+        {
+            name: "Eye Disease Prediction",
+            image: EYE,
+            content: "I utilized ResNet, EfficientNet, Inception, and MobileNet neural network models to predict eye diseases, employing transfer learning techniques for improved accuracy and conducting thorough evaluations to identify the optimal predictive models.",
+            github:"https://github.com/lalitmohit/Eye-Disease-Prediction",
+            visit:"https://github.com/lalitmohit/Eye-Disease-Prediction"
+        },
+
+        {
             name: "Web App for Flipr Hackathon",
             image: Stock,
             content: "It is a hackathon web app that I made for the Flipr Hackathon. It is a full stack web app that I made using React, Node.js, Express.js, and MongoDB. It is a platform where users can see stock prices for diff. Org's.",
@@ -49,13 +92,7 @@ const Projects = () => {
             github:"https://github.com/lalitmohit/PassGen",
             visit:"https://github.com/lalitmohit/PassGen"
         },
-        {
-            name: "Web App for OpenLake",
-            image: Openlake,
-            content: "I developed a web application for Openlake, an open-source club at IIT Bhilai, using HTML, CSS, JavaScript, React, and FAST API to enhance user experience and engagement within the Openlake community through intuitive and responsive web functionalities.",
-            github:"https://github.com/lalitmohit/OpeLake-Website-by-DevDynamos",
-            visit:"https://webwave-test.vercel.app/"
-        },
+        
         // {
         //     name: "Structural Strength Prediction",
         //     image: Gensec,
@@ -63,42 +100,10 @@ const Projects = () => {
         //     github:"https://github.com/lalitmohit/Structural_Strength_Prediction",
         //     visit:"https://github.com/lalitmohit/Structural_Strength_Prediction"
         // },
-        {
-            name: "Eye Disease Prediction",
-            image: EYE,
-            content: "I utilized ResNet, EfficientNet, Inception, and MobileNet neural network models to predict eye diseases, employing transfer learning techniques for improved accuracy and conducting thorough evaluations to identify the optimal predictive models.",
-            github:"https://github.com/lalitmohit/Eye-Disease-Prediction",
-            visit:"https://github.com/lalitmohit/Eye-Disease-Prediction"
-        },
-        {
-            name: "Emotion Prediction by Text",
-            image: Emotion,
-            content: "I employed CNN and NLP techniques to develop a project that predicts emotions from textual data, utilizing advanced machine learning algorithms for accurate emotion prediction.",
-            github:"https://github.com/lalitmohit/Emotion-Detection-by-Text",
-            visit:"https://emotion-detection-by-text-2r7exvwxrtjbzdsh9xr8l5.streamlit.app/"
-        },
-        {
-            name: "Campus Delivery System ( Pharmacy )",
-            image: Aoushadhi,
-            content: "I led the development of a feature-rich online pharmacy platform similar to Netmeds, creating a user-friendly interface with React.js for easy navigation, collaborating with vendors to upload detailed medicine information, integrating secure payment gateways, and leveraging Node.js and Express.js for a robust backend to manage authentication, authorization, and database operations.",
-            github:"https://github.com/lalitmohit/Aoushadi-Frontend",
-            visit:"https://ecomm-med.vercel.app"
-        },
-        {
-            name: "TA Management",
-            image: TA,
-            content: "I led the development of the TA Management System, integrating frontend, backend, and ML models, designed to assist professors in selecting suitable TAs, establishing user authentication, categorizing user information for efficient storage, implementing a user-friendly interface with React.js, and developing an ML model using Metaflow, Sentence-transformer, and NLP for accurate TA assessment.",
-            github:"https://github.com/lalitmohit/TA_Management_CSD_Frontend",
-            visit:"https://ta-management-csd-frontend.vercel.app"
-        },
-        {
-            name: "MY Portfolio",
-            image: Portfolio,
-            content: "I developed my portfolio using React and Material-UI, showcasing my projects and skills in a visually appealing, responsive design. This project demonstrates my expertise in front-end development, intuitive UI creation, and proficiency with modern web technologies.",
-            github:"https://github.com/lalitmohit/MUI",
-            visit:"https://mui-seven-chi.vercel.app/"
-        },
-
+        
+        
+        
+        
 
         
     ]
